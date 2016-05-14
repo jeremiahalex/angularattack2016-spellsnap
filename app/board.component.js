ce9b7acb90c0754f2df40e963a2781000dba101d
Jeremiah Alexander <jeremiah@ideonic.com>
2016-05-14 11:23:45 +0800

    replace gridcell number with object

--- app/board.component.js
+++ app/board.component.js
@@ -27,38 +27,38 @@ System.register(['@angular/core'], function(exports_1, context_1) {
                     for (var y = 0; y < this.rowCount; ++y) {
                         this.gridCells[y] = [];
                         for (var x = 0; x < this.columnCount; ++x) {
-                            this.gridCells[y][x] = 0;
+                            this.gridCells[y][x] = { x: x, y: y, content: 0 };
                             //temp - we will read the cell values from the server but for now we're just testing our grid, so manuall add 
                             if (x % 3 === 1 && y % 3 === 1) {
                                 //a possible starter cell
-                                this.gridCells[y][x] = 1;
+                                this.gridCells[y][x].content = 1;
                             }
                             if (x === 1 && y === 1)
-                                this.gridCells[y][x] = 20; //S
+                                this.gridCells[y][x].content = 20; //S
                             else if (x === 2 && y === 1)
-                                this.gridCells[y][x] = 17; //P
+                                this.gridCells[y][x].content = 17; //P
                             else if (x === 3 && y === 1)
-                                this.gridCells[y][x] = 16; //E
+                                this.gridCells[y][x].content = 16; //E
                             else if (x === 4 && y === 1)
-                                this.gridCells[y][x] = 13; //L
+                                this.gridCells[y][x].content = 13; //L
                             else if (x === 5 && y === 1)
-                                this.gridCells[y][x] = 13; //L
+                                this.gridCells[y][x].content = 13; //L
                             else if (x === 1 && y === 2)
-                                this.gridCells[y][x] = 15; //N
+                                this.gridCells[y][x].content = 15; //N
                             else if (x === 1 && y === 3)
-                                this.gridCells[y][x] = 2; //A
+                                this.gridCells[y][x].content = 2; //A
                             else if (x === 1 && y === 4)
-                                this.gridCells[y][x] = 17; //P
+                                this.gridCells[y][x].content = 17; //P
                         }
                     }
                 }
                 BoardComponent.prototype.cellClicked = function (cell) {
-                    console.log('cell clicked: ' + cell);
+                    console.log("cell clicked at x: " + cell.x + ", y: " + cell.x + ". Content is: " + cell.content);
                 };
                 BoardComponent = __decorate([
                     core_1.Component({
                         selector: 'game-board',
-                        template: "\n        <section class=\"board\">\n            <div *ngFor=\"let row of gridCells\" class=\"board-row\">\n                <div *ngFor=\"let cell of row\" (click)=\"cellClicked(cell)\"  class=\"board-cell\">\n                    {{alphabet.charAt(cell)}}\n                </div>\n            </div>\n        </section>\n    "
+                        template: "\n        <section class=\"board\">\n            <div *ngFor=\"let row of gridCells\" class=\"board-row\">\n                <div *ngFor=\"let cell of row\" (click)=\"cellClicked(cell)\"  class=\"board-cell\">\n                    {{alphabet.charAt(cell.content)}}\n                </div>\n            </div>\n        </section>\n    "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], BoardComponent);
--- app/board.component.ts
+++ app/board.component.ts
@@ -1,12 +1,18 @@
 import {Component} from '@angular/core';
 
+interface GridCell {
+  y: number;
+  x: number;
+  content: number;
+}
+
 @Component({
     selector: 'game-board',
     template: `
         <section class="board">
             <div *ngFor="let row of gridCells" class="board-row">
                 <div *ngFor="let cell of row" (click)="cellClicked(cell)"  class="board-cell">
-                    {{alphabet.charAt(cell)}}
+                    {{alphabet.charAt(cell.content)}}
                 </div>
             </div>
         </section>
@@ -16,7 +22,7 @@ export class BoardComponent {
     rowCount : number = 9;
     columnCount : number = 9;
     
-    gridCells : number[][];
+    gridCells : GridCell[][];
     
     alphabet : string = ' *ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     
@@ -26,26 +32,26 @@ export class BoardComponent {
         for ( let y = 0; y < this.rowCount; ++y){
             this.gridCells[y] = [];
             for ( let x = 0; x < this.columnCount; ++x){
-                this.gridCells[y][x] = 0;
+                this.gridCells[y][x] = { x: x, y: y, content: 0};
                 //temp - we will read the cell values from the server but for now we're just testing our grid, so manuall add 
                 if ( x % 3 === 1 && y % 3 === 1 ){
                     //a possible starter cell
-                    this.gridCells[y][x] = 1;
+                    this.gridCells[y][x].content = 1;
                 }
                 
-                if ( x === 1 && y === 1 ) this.gridCells[y][x] = 20; //S
-                else if ( x === 2 && y === 1 ) this.gridCells[y][x] = 17; //P
-                else if ( x === 3 && y === 1 ) this.gridCells[y][x] = 16; //E
-                else if ( x === 4 && y === 1 ) this.gridCells[y][x] = 13; //L
-                else if ( x === 5 && y === 1 ) this.gridCells[y][x] = 13; //L
-                else if ( x === 1 && y === 2 ) this.gridCells[y][x] = 15; //N
-                else if ( x === 1 && y === 3 ) this.gridCells[y][x] = 2; //A
-                else if ( x === 1 && y === 4 ) this.gridCells[y][x] = 17; //P
+                if ( x === 1 && y === 1 ) this.gridCells[y][x].content =  20; //S
+                else if ( x === 2 && y === 1 ) this.gridCells[y][x].content =  17; //P
+                else if ( x === 3 && y === 1 ) this.gridCells[y][x].content =  16; //E
+                else if ( x === 4 && y === 1 ) this.gridCells[y][x].content =  13; //L
+                else if ( x === 5 && y === 1 ) this.gridCells[y][x].content =  13; //L
+                else if ( x === 1 && y === 2 ) this.gridCells[y][x].content =  15; //N
+                else if ( x === 1 && y === 3 ) this.gridCells[y][x].content =  2; //A
+                else if ( x === 1 && y === 4 ) this.gridCells[y][x].content =  17; //P
             }
         }
     }
     
-    cellClicked(cell: number) {
-        console.log('cell clicked: ' + cell);
+    cellClicked(cell: GridCell) {
+        console.log(`cell clicked at x: ${cell.x}, y: ${cell.x}. Content is: ${cell.content}`);
     }
 }
